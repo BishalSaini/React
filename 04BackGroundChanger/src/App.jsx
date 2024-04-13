44eab7a4460672f99b2bd2ec79727e25722b8e24
BishalSaini <vishal32saini@gmail.com>
2024-04-13 15:39:26 +0530

    Mini-Project

--- 04BackGroundChanger/src/App.jsx
+++ 04BackGroundChanger/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <> 
     <h1>BackGround Color Changer- Mini Project</h1>
     <div className="main-div" style={{ backgroundColor: color }}><p>Color Changes To {color}</p></div>  
-    <button onClick={()=> setColor("Red")} style={{backgroundColor:"Red"}}>Red</button>   
+    <button onClick={()=> setColor("Red")} style={{backgroundColor:"Red"}}>Red</button>   {/*  onClick we cannot pass directly the value setColor("red") onClick Takes a Function in this we are passing function with parameter so it will not work so will be use aour callback */}
     <button onClick={()=> setColor("White")} style={{backgroundColor:"white", color:"black"}}>White</button>   
     <button onClick={()=> setColor("Green")} style={{backgroundColor:"Green"}}>Green</button>   
     <button onClick={()=> setColor("Blue")} style={{backgroundColor:"Blue"}}>Blue</button>   
